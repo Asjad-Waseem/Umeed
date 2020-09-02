771d6112432cb473f47b012312b240ea5a391847
Asjad-Waseem <70429177+Asjad-Waseem@users.noreply.github.com>
2020-09-03 01:20:23 +0500

    Update store.js
    Redux Dev Tools Change Version 1.1

--- src/store.js
+++ src/store.js
@@ -9,7 +9,8 @@ const initialState = {};
 const middleware = [thunk];
 
 // const store = createStore(authReducer, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+// COMMENTED LINE export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()));
 // const store = createStore(userReducer, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 // console.log(store.getState());
 
