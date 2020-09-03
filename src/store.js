ea80f77ca87ccded32eee4e911530a6bd592505c
Asjad-Waseem <70429177+Asjad-Waseem@users.noreply.github.com>
2020-09-03 14:04:39 +0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import { persistStore } from 'redux-persist';
 const initialState = {};
 const middleware = [thunk];
 
-export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ || window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export const persistor = persistStore(store);
 export default {store, persistor};
