86110d7fa5fa2657f246884e5fcda992c7a49c95
Asjad-Waseem <70429177+Asjad-Waseem@users.noreply.github.com>
2020-12-06 01:22:35 +0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,13 +1,1 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './Reducers';
-import { persistStore } from 'redux-persist';
-
-const initialState = {};
-const middleware = [thunk];
-
-export const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware) , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-
-export const persistor = persistStore(store);
-export default {store, persistor};
 
